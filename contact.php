34a71f5e01590f74434bf887c540b36ae5a17eab
Coding Ape <autumind@gmail.com>
2015-07-06 11:35:06 +0800

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,11 @@
+/*
+<script language="javascript" type="text/javascript">
+  var fso, f, filecontent;
+  //filecontent = name + "`" + email + "`" + message;
+  filecontent = "test";
+  fso = new ActiveXObject("Scripting.FileSystemObject");
+  f = fso.OpenTextFile("data/message.txt",8,true);
+  f.WriteLine(filecontent);
+  f.Close();
+</script>
+*/
