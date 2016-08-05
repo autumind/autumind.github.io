28b62d72cd920eaa9742d2c945ad316436d9966c
reno.shen <autumind@gmail.com>
2016-08-05 09:18:25 +0800

    cv

---
+++ asset/js/ch.js
@@ -0,0 +1,180 @@
+var ch = {
+	name     : "沈志兵",
+	title    : "高级开发工程师",
+	desc     : ["和绝大多数志愿长大成为科学家的小朋友一样，毫无意外地没有成为科学家，我成为了程序员。" , 
+		        "在圈内这听起来好像有点绝望，但是从农村包围城市的战略上讲，这也是一种胜利。" , 
+		        "毕业至今四年多一点，与代码遭遇的点滴虽然没有历历在目，却也没有成为过眼烟云。" , 
+		        "不是技术大牛，总也朝着全栈工程师的方向努力；不是技术狂热分子，却对解决问题有着无比的执着；不是代码终结者，但也想让代码更美丽。" , 
+		        "热爱生活，希望通过努力可以让自己去超市不看价格；喜欢冒险，一直想去蹦极或者跳伞；崇尚简单，我所拥有的很少超过两种颜色。" , 
+		        "偶尔张狂，偶尔深沉，不一定能飞黄腾达，但也会不遗余力的把这一生过得更丰富多彩。"],
+	quote    : "其实要是用几行字就能概括我，我会没脸活下去的 :-)",
+	job      : [
+					{
+						company  : "江苏优客互联科技股份有限公司",
+						start    : "2015年12月",
+						end      : "至今",
+						position : "高级Java开发工程师",
+						content  : ["在司半年至今，参与开发了微信企业号微移云产品、年轻妈妈APP。",
+									"微信企业号主要面向企业用户，涵盖了企业管理中的考勤，请假，通知，报销，审批等常用流程，并提供定制服务，开发涉及SpringMVC、MySQL、H5、CSS3、Javascript、缓存、nginx等技术；",
+									"年轻妈妈APP主要面向怀孕及刚生育妈妈的在线问诊并提供孕食推荐，月子会所订购等服务，开发涉及除微移云相关以外，还涉及H5 Hybird、DCloud壳应用、H5PLUS等技术。"]
+					},
+					{
+						company  : "富士通南大软件技术有限公司",
+						start    : "2012年12月",
+						end      : "2015年11月",
+						position : "普通开发",
+						content  : ["在司三年，前一年半时间主要参与对日项目，其中包括物流系统，订单系统，涉及Action Script 3、Java、SQL等相关技术，并且参与了一些UI，SS设计。",
+									"其中出差日本期间，掌握了一种有富士通自有主导的开发语言PRO IV，并参与开发了Glovia相关功能，之后作为技术顾问对后参与同事进行了培训。",
+									"后一半时间主要参与国内项目，包括某市旅游推介及点评系统，中小型制造业出入库管理系统，政府单位行政处罚系统，某幼儿园资源管理系统，域名拍卖系统，涉及SpringMVC、H5、CSS3、SQL、Maven、微信平台等相关技术。"]
+					},
+					{
+						company  : "瞬联科技",
+						start    : "2012年07月",
+						end      : "2012年11月",
+						position : "Android系统开发工程师",
+						content  : ["在司4个月期间，参加Android Framework相关培训，并且参与了SMS模块双卡双待的代码改造。"]
+					}
+				],
+	edu      : [
+					{
+						school : "南京农业大学",
+						start  : "2008年09月",
+						end    : "2012年06月",
+						major  : "计算机科学与技术",
+						degree : "学士",
+						where  : "江苏，南京",
+						desc   : ["整个大学期间，学习为主，基础课程基本高分通过，毕业绩点班级前三。",
+								  "作为新闻记者参加过社团，获得过国家、校级等各类奖助学金。",
+								  "为学校2012年优秀毕业生。"]
+					}
+				],
+	skill    : [
+					{
+						set_name   : "编程技能",
+						set_type   : "c",
+						set_skills : [
+							{
+								name    : "Java",
+								level   : "",
+								percent : "80"
+							},
+							{
+								name    : "SQL",
+								level   : "",
+								percent : "90"
+							},
+							{
+								name    : "JavaScript",
+								level   : "",
+								percent : "80"
+							},
+							{
+								name    : "H5&CSS3",
+								level   : "",
+								percent : "70"
+							},
+							{
+								name    : "Linux&Unix",
+								level   : "",
+								percent : "60"
+							}
+						]
+					},
+					{
+						set_name   : "语言技能",
+						set_type   : "l",
+						set_skills : [
+							{
+								name    : "英语",
+								level   : "CET-6",
+								percent : "80"
+							},
+							{
+								name    : "日语",
+								level   : "JLPT-N3",
+								percent : "60"
+							}
+						]
+					},
+					{
+						set_name   : "其他技能",
+						set_type   : "o",
+						set_skills : [
+							{
+								name    : "Office",
+								level   : "",
+								percent : "80"
+							},
+							{
+								name    : "设计",
+								level   : "",
+								percent : "40"
+							}
+						]
+					}
+				],
+	strength : [
+					"团队精神",
+					"严谨的逻辑思考",
+					"责任心强",
+					"旅游",
+					"电影"
+				],
+	contact  : [
+// icon 参照http://amazeui.org/css/icon
+					{
+						name      : "邮件",
+						icon      : "envelope",
+						siteOrNum : "autumind@gmail.com"
+					},
+					{
+						name      : "手机",
+						icon      : "mobile",
+						siteOrNum : "+8615895973966"
+					},
+					{
+						name      : "QQ",
+						icon      : "qq",
+						siteOrNum : "978233845"
+					},
+					{
+						name      : "微信",
+						icon      : "wechat",
+						siteOrNum : "autumind"
+					},
+					{
+						name      : "地址",
+						icon      : "location-arrow",
+						siteOrNum : "江苏省南京市  " +
+						            "秦淮区 双乐园小区18栋81单元701室  " +
+						            "210004"
+					}
+				],
+	social: [
+				{
+					name      : "主页",
+					icon      : "home",
+					siteOrNum : "http://www.iylyi.com"
+				},
+				{
+					name      : "GitHub",
+					icon      : "github",
+					siteOrNum : "https://github.com/autumind"
+				},
+				{
+					name      : "SO",
+					icon      : "stack-overflow",
+					siteOrNum : "http://stackoverflow.com/users/5960800/jpg"
+				},
+				{
+					name      : "微博",
+					icon      : "weibo",
+					siteOrNum : "http://weibo.com/autumind"
+				},
+				{
+					name      : "豆瓣",
+					icon      : "douban",
+					siteOrNum : "https://www.douban.com/people/autumind/"
+				}
+			]
+};
