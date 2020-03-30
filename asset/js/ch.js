876d34d5bf187605fa0631d1b5f3865b97637a13
Blank <autumind@gmail.com>
2020-03-30 13:28:52 +0800

    Update ch.js

--- asset/js/ch.js
+++ asset/js/ch.js
@@ -10,13 +10,13 @@ var ch = {
     quote: "成年人的世界永远没有那么简单",
     job: [
         {
-            company: "Shein",
+            company: "南京希音电子商务有限公司",
             start: "2018年03月",
             end: "至今",
             position: "资深JAVA开发工程师、项目经理",
-            content: ["在司期间，前期主要参与某市集团商场O2O运营系统的开发；后期主要参与了基于微服务的系统架构改造并主导完成了订单服务开发。",
-                "公司业务提供购物中心商业运营平台建设、业务运营、大数据分析、精准营销、会员经营、智慧商管、商户服务等全系列购物中心智慧运营互联网+解决方案。",
-                "老O2O系统主要基于SpringMVC+SpringJPA实现。现已基于SpringCloud、SpringBoot完成了重新架构，另涉及Mybatis、Redis、docker、RocketMQ、Activiti等技术整合。"]
+            content: ["在司期间，主要参与SHEIN供应链业务线相关模块的开发设计工作，包括财务-应收、财务-应付、物资、接单等。",
+                "SHEIN专注于女性快时尚的跨境B2C，以快时尚女装为主体，涉及服装生产、采购、库存、海外物流、海外网站及线下销售等业务。",
+                "2C系统主要以PHP技术方案为主，如：MALL、OMS，2B系统以JAVA技术体系基于Springboot、Redis、MySQL、ES、RabbitMQ等实现的微服务架构，如：财务系统、供应链各模块系统。"]
         }
         , {
             company: "南京墨博云舟信息科技有限公司",
